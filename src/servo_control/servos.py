3672639d22f46a4fda0c4dab9caf208ce24afd3a
Pascal Ernst <p.ernst@ostfalia.de>
2015-02-15 16:21:46 +0100

    Add the source-files

---
+++ src/servo_control/servos.py
@@ -0,0 +1,74 @@
+"""Defines the existing servos and their data
+"""
+
+class Servo(object):
+    """Represents a pwm-controlled servo"""
+    def __init__(self):
+        self.range_min = 219   # 1ms wide pulse
+        self.range_max = 438   # 2ms wide pulse
+        self.pwm_port = 0   # The port the servo can be accessed from
+
+    def pwm_range_adjust(self, percmin, permax):
+        """allows to change the pwm-range
+
+        Useful when not the full movement of the servo is used
+        Arguments:
+        percmin -- percentage of the range minimum. Giving 0.5 as percmin
+            would half the range by setting the range_min to the middle of the
+            range
+        percmax -- percentage-difference of the maximum range. -''-
+        """
+        pwm_range = self.range_max - self.range_min
+        self.range_min = ((1 - percmin) * pwm_range) + self.range_min
+        self.range_max = self.range_max - ((1 - percmax) * pwm_range)
+
+
+class Esc(Servo):
+    """The esc of the Summit"""
+    def __init__(self):
+        super(Esc, self).__init__()
+        self.pwm_port = 4
+
+
+class Steering1(Servo):
+    """The steering servos of the Summit"""
+    def __init__(self):
+        super(Steering1, self).__init__()
+        self.pwm_port = 0
+
+
+class Steering2(Servo):
+    """The steering servos of the Summit"""
+    def __init__(self):
+        super(Steering2, self).__init__()
+        self.pwm_port = 1
+
+
+class RearLocker(Servo):
+    """The rear locker servo of the Summit"""
+    def __init__(self):
+        super(RearLocker, self).__init__()
+        self.pwm_port = 10
+        self.pwm_range_adjust(1, 0.9)
+
+
+class FrontLocker(Servo):
+    """The front locker servo of the Summit"""
+    def __init__(self):
+        super(FrontLocker, self).__init__()
+        # The servo needs to be operated at a smaller range
+        self.range_min = self.range_min * 1.1
+        self.range_max = self.range_max *
+        self.pwm_port = 9
+        # This servos movement is quite restricted
+        self.pwm_range_adjust(0.7, 0.9)
+
+
+class Transmission(Servo):
+    """The transmission servo of the Summit"""
+    def __init__(self):
+        super(Transmission, self).__init__()
+        self.pwm_port = 8
+        # Not really sure if this keeps the transmission in place, but this
+        # should do it without putting too much pressure on the servo
+        self.pwm_range_adjust(0.9, 0.8)
---
+++ src/servo_control/summit_pwm.py
@@ -0,0 +1,14 @@
+from lib.adafruit_servo_driver.adafruit_servo_driver import PWM
+import servos
+
+
+class SummitPWM(PWM):
+	"""Sets values allowing to control the Traxxas Summits servos"""
+    def __init__(self):
+        super(SummitPWM, self).__init__(0x40, debug=False)
+        self.setPWMFreq(50)
+
+    def servo_set(self, servo, range_percentage):
+        diff = (servo.range_max - servo.range_min) * range_percentage
+        val = servo.range_min + diff
+        self.setPWM(servo.pwm_port, 0, val)
