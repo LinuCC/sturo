da58f4cf1da11230cbe5c87c10800bea81d4fa5e
Pascal Ernst <pascal.cc.ernst@gmail.com>
2015-11-25 12:41:19 +0000

    Smaller fixes for Driver

--- src/driver.py
+++ src/driver.py
@@ -2,31 +2,32 @@ import sturo
 
 class Driver(object):
 
-    class Mode(Object):
+    class Mode(object):
         FORWARD = 1
         SLIGHT_ADJUST_RIGHT = 2
         ADJUST_RIGHT = 3
+	RESET = 4
 
     def __init__(self):
-        self.sturo = Sturo()
+        self.sturo = sturo.Sturo()
         self.sturo.set_speed(0)
         self.sturo.set_steering(0)
         self.sturo.set_transmission(0)
         self.sturo.set_front_locker(0)
         self.sturo.set_rear_locker(0)
-        self.drive_mode =
+        self.drive_mode = self.Mode.FORWARD
 
     def auto_drive(self):
         drive_on = True
         while(drive_on):
-            mode = self.check_sensors()
+            mode = self.evaluate()
             self.drive(mode)
 
     def evaluate(self):
-        return Mode.FORWARD
+        return self.Mode.FORWARD
 
     def drive(self, mode):
-        if mode == Mode.FORWARD:
+        if mode == self.Mode.FORWARD:
             self.sturo.set_speed(10)
             self.sturo.set_steering(0)
 
